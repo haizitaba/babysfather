824653ef972ed3b0e4cfa310acb1b6037862fc6c
learningspider <1161192890@qq.com>
2017-04-14 21:24:42 +0800

    mm

---
+++ library/imports/f2/f2ee9a42-b829-4104-a01c-52ad5366c6b4.js
@@ -0,0 +1,42 @@
+"use strict";
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,      // The default value will be used only when the component attaching
+        //                           to a node for the first time
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+    },
+
+    // use this for initialization
+    onLoad: function onLoad() {
+        var clickEventHandler = new cc.Component.EventHandler();
+        clickEventHandler.target = this.node; //这个 node 节点是你的事件处理代码组件所属的节点
+        clickEventHandler.component = "MyComponent"; //这个是代码文件名
+        clickEventHandler.handler = "callback";
+        clickEventHandler.customEventData = "foobar";
+
+        var button = node.getComponent(cc.Button);
+        button.clickEvents.push(clickEventHandler);
+    },
+
+    callback: function callback(event, customEventData) {
+        //这里 event 是一个 Touch Event 对象，你可以通过 event.target 取到事件的发送节点
+        var node = event.target;
+        var button = node.getComponent(cc.Button);
+        //这里的 customEventData 参数就等于你之前设置的 "foobar"
+    }
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
