6bcc91ca6ff69d5728263879ef9f1bebad765106
learningspider <1161192890@qq.com>
2017-04-19 20:49:45 +0800

    rnd

--- assets/js/buttonproperty.js
+++ assets/js/buttonproperty.js
@@ -16,14 +16,53 @@ cc.Class({
 
     // use this for initialization
     setProperty: function(){
-       var userData = {
-    name: 'Tracer',
-    level: 1,
-    gold: 100
-};
+   var userData = {
+        name: 'Tracer',
+        level: 1,
+        gold: 100
+    };
+    
+    var baidu_developer = {
+        name: 'Tracer',
+        rate: 2,
+        salary: 10000
+    };
 
-cc.sys.localStorage.setItem('userData', JSON.stringify(userData));
-console.log("按钮按下")
+    cc.sys.localStorage.setItem('userData', JSON.stringify(userData));
+    console.log("按钮按下");
+    },
+
+    // function rnd(min, max) {     return parseInt(Math.random()*(max - min + 1) + min);   } 
+    rnd: function(min,max){
+        return parseInt(Math.random()*(max - min + 1) + min);
+    },
+    
+    contains:function(a,obj){
+        for (var i = 0; i < a.length; i++) {
+            if (a[i] == obj) {
+                return true;
+            }
+        }
+        return false;
+    },
+    
+    jobhunting: function(){
+        var job = ["baidu_developer","fenghua_developer","ali_developer","tenant_developer","gongfu_developer"];
+        var arr = [];
+        while(arr.length < 3){
+            var rand = this.rnd(0,job.length);
+            if(this.contains(arr, rand)){
+                
+            }else{
+                arr.push(rand);
+            }
+        }
+        for(var j=0;j<arr.length;j++) 
+        {
+            var i = j+1
+            console.log(job[i]);
+        } 
+        
     }
 
     // called every frame, uncomment this function to activate update callback
